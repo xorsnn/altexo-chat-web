6f8e8fb7ac12031cd7fcd435d32c54b2f383ff55
Konstantin Kopkov <pryzm0@gmail.com>
2016-11-23 17:51:44 +0400

    feat: continuosly ping extension client until pong received

--- src/extension/content-script.js
+++ src/extension/content-script.js
@@ -14,14 +14,20 @@ port.onMessage.addListener(function(msg) {
 	window.postMessage(msg, '*');
 });
 
+var pingInterval = setInterval(function() {
+  window.postMessage({ type: 'SS_PING', text: 'start' }, '*');
+}, 500);
+
 window.addEventListener('message', function(event) {
-	// We only accept messages from ourselves
-	if (event.source != window) return;
+  // We only accept messages from ourselves
+  if (event.source != window) return;
 
-	if (event.data.type && ((event.data.type === 'SS_UI_REQUEST') ||
-							(event.data.type === 'SS_UI_CANCEL'))) {
-		port.postMessage(event.data);
-	}
+  if (event.data.type && ((event.data.type === 'SS_UI_REQUEST') ||
+              (event.data.type === 'SS_UI_CANCEL'))) {
+    port.postMessage(event.data);
+  }
+
+  if (event.data.type && event.data.type == 'SS_UI_PONG') {
+    clearInterval(pingInterval);
+  }
 }, false);
-
-window.postMessage({ type: 'SS_PING', text: 'start' }, '*');
