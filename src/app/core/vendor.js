38fa21d7bf6477ab46d3b64748cde160446ddf1e
Konstantin Kopkov <pryzm0@gmail.com>
2017-01-09 17:02:26 +0400

    fix: move some imports

--- src/app/core/vendor.js
+++ src/app/core/vendor.js
@@ -4,12 +4,14 @@ module.exports = function () {
   /* JS */
   global.$ = global.jQuery = require('jquery');
   require('angular');
-  require('angular-animate')
-  require('angular-aria')
-  require('angular-messages')
+  require('angular-animate');
+  require('angular-aria');
+  require('angular-messages');
   require('angular-material');
   require('angular-route');
+  require('angular-sanitize');
   require('lodash');
+  require('ngstorage');
 
   // TODO: take a look at this workaround later
   var adapter = require('../../../bower_components/webrtc-adapter/adapter.js');
@@ -17,6 +19,7 @@ module.exports = function () {
     global[attrname] = adapter[attrname];
   }
 
+  global.THREE = require('../../../bower_components/three.js/build/three.js');
   global.kurentoUtils = require('../../../bower_components/kurento-utils/dist/kurento-utils.js');
   require('../../../bower_components/angular-promisify/dist/denodeify.js');
 
--- src/app/index.coffee
+++ src/app/index.coffee
@@ -1,8 +1,5 @@
 'use strict'
 
-require('ngstorage')
-require('angular-sanitize')
-
 Raven.config(AL_SENTRY_ENDPOINT, {
   ignoreUrls: [ /<raven_urls_to_ignore>/ ]
 } ).addPlugin(require('raven-js/plugins/angular'), angular).install()
